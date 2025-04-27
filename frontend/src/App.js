fedb433906b3fb2ccc7cdbd58cb3b0d8eba6d425
Laiba <115210630+Laiba51-CS@users.noreply.github.com>
2025-04-28 00:12:56 +0500

    chnages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,6 @@
-import './App.css';
-<<<<<<< HEAD
-import LandingPage from './Components/landingpage'; // Ensure correct case for component names
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Add more routes here if needed */}
-        </Routes>
-      </div>
-    </Router>
-=======
-import Footer from './components/footer.jsx';
-import Card from './components/Card.jsx';
+import Footer from './Components/footer.jsx';
+import Card from './Components/Card.jsx';
 import landingPageImage from './assets/landing_page_image.jpg';
 function App() {
   return (
@@ -28,7 +13,6 @@ function App() {
       />
       <Footer />
     </div>
->>>>>>> a92ff1ae99e7730a827dab118d629590d9d34a9b
   );
 }
 
