c7a8331cf9bdd95a1bea0e7c8c2190357892c58b
Bilal Qamar <bilalqamar229@gmail.com>
2025-04-28 13:00:43 +0500

    landingpage css changes

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './navbar.css' // Import the CSS file for styling
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css'; // Import the CSS file for styling
 
 export default function Navbar() {
     return (
         <div className='background'>
             <nav className="navbar">
                 <div className="navbar-logo">
-                    <Link to="/">Marble Polishing</Link>
+                    <Link to="/">تلميع الرخام</Link>
                 </div>
                 <ul className="navbar-links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/services">Tile Polishing</Link></li>
-                    <li><Link to="/about">Ceramic Polishing</Link></li>
-                    <li><Link to="/about">Marble Polishing</Link></li>
-                    <li><Link to="/about">Procelain Polishing</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/">الرئيسية</Link></li>
+                    <li><Link to="/services">تلميع البلاط</Link></li>
+                    <li><Link to="/about">تلميع السيراميك</Link></li>
+                    <li><Link to="/about">تلميع الرخام</Link></li>
+                    <li><Link to="/about">تلميع البورسلين</Link></li>
+                    <li><Link to="/contact">اتصل بنا</Link></li>
                 </ul>
             </nav>
-                <hr />
+            <hr />
         </div>
-            
-    )
+    );
 }
